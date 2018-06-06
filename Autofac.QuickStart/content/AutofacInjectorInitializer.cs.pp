b4067fed4ce8f216ad26c27eec8b1e1021c9a1b0
Trinath Reddy Inturi <TrinathInturi@users.noreply.github.com>
2018-06-06 19:24:15 +0530

    Add files via upload

--- Autofac.QuickStart/content/AutofacInjectorInitializer.cs.pp
+++ Autofac.QuickStart/content/AutofacInjectorInitializer.cs.pp
@@ -1,12 +1,12 @@
-﻿[assembly: WebActivator.PostApplicationStartMethod(typeof($rootnamespace$.AutofacInjectorInitializer), "Initialize")]
-
-namespace $rootnamespace$
+﻿namespace $rootnamespace$
 {
     using System.Reflection;
     using System.Web.Mvc;
     using Autofac;
     using Autofac.Integration.Mvc;
     using System;
+	using System.Web.Http;
+    using Autofac.Integration.WebApi;
      /// <summary>
     /// Autofact initializer
     /// </summary>
@@ -14,34 +14,89 @@ namespace $rootnamespace$
     {
 
         /// <summary>
-        /// Initialize the container and register it as MVC4 Dependency Resolver.
+        /// Initialize the container
         /// </summary>
-        public static IContainer Initialize()
+		public static void Initialize(HttpConfiguration config)
         {
-
+            Initialize(config, RegisterServices(new ContainerBuilder()));
+        }
+		/// <summary>
+        /// Set the Dependency Resolver
+        /// </summary>
+        public static void Initialize(HttpConfiguration config, IContainer container)
+        {
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+        private static IContainer RegisterServices(ContainerBuilder builder)
+        {
+	
             // Did you know the container can diagnose your configuration? http://docs.autofac.org/en/latest/integration/mvc.html
-            var containerBuilder = new ContainerBuilder();
-
-            InitializeContainer(containerBuilder);
-
-            // Register your MVC controllers.
+           
+		    // Register your MVC controllers.
             containerBuilder.RegisterControllers(typeof(MvcApplication).Assembly);
 
+			//Register your API controllers
+			builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+			//Register as TYPE: (Registering Components and Services)
+			builder.RegisterType<ClassName>().As<InterfaceName>().InstancePerRequest();
+			
+			//OPTIONAL: (Another Example)
+			builder.RegisterType<ConsoleLogger>();
+
+			//OPTIONAL: Specifying a Constructor
+			builder.RegisterType<MyComponent>().UsingConstructor(typeof(ILogger), typeof(IConfigReader));
+
+			//OPTIONAL: Registering an instance
+			var output = new StringWriter();builder.RegisterInstance(output).As<TextWriter> //To Control Disposal of Object use '.ExtenallyOwned()' at the end 
+
+			//Conditional Registration is Added in Autofac 4.4
+
+			//OPTIONAL: Conditional Registration 'OnlyIf()'
+			builder.RegisterType<Manager>().As<IManager>().OnlyIf(reg =>reg.IsRegistered(new TypedService(typeof(IService))) && reg.IsRegistered(new TypedService(typeof(HandlerB))));
+
+			//OPTIONAL: Conditional Registration 'IfNotRegistered()'
+			builder.RegisterType<HandlerA>().AsSelf().As<IHandler>().IfNotRegistered(typeof(HandlerB));
+
+			//OPTIONAL: Passing Parameter to Register
+			
+			// Using a 'NAMED Parameter'
+			builder.RegisterType<ConfigReader>().As<IConfigReader>().WithParameter("configSectionName", "sectionName");
+
+			// Using a 'TYPED Parameter':
+			builder.RegisterType<ConfigReader>().As<IConfigReader>().WithParameter(new TypedParameter(typeof(string), "sectionName"));
+
+			// Using a 'RESOLVED Parameter':
+			builder.RegisterType<ConfigReader>().As<IConfigReader>().WithParameter(
+				new ResolvedParameter((pi, ctx) => pi.ParameterType == typeof(string) && pi.Name == "configSectionName",(pi, ctx) => "sectionName")
+			);
+
+			//OPTIONAL: Property injection
+			builder.RegisterType<A>().WithProperty("PropertyName", propertyValue);
+
+			//OPTIONAL: Method injection
+			builder.Register<MyObjectType>().OnActivating(e => { 
+					var dep = e.Context.Resolve<TheDependency>();
+					e.Instance.SetTheDependency(dep);
+			 });
+
+
             // OPTIONAL: Register model binders that require DI.
-            containerBuilder.RegisterModelBinders(Assembly.GetExecutingAssembly());
-            containerBuilder.RegisterModelBinderProvider();
+            builder.RegisterModelBinders(Assembly.GetExecutingAssembly());
+            builder.RegisterModelBinderProvider();
 
             // OPTIONAL: Register web abstractions like HttpContextBase.
-            containerBuilder.RegisterModule<AutofacWebTypesModule>();
+            builder.RegisterModule<AutofacWebTypesModule>();
 
             // OPTIONAL: Enable property injection in view pages.
-            containerBuilder.RegisterSource(new ViewRegistrationSource());
+            builder.RegisterSource(new ViewRegistrationSource());
 
             // OPTIONAL: Enable property injection into action filters.
-            containerBuilder.RegisterFilterProvider();
+            builder.RegisterFilterProvider();
 
 
-            var container = containerBuilder.Build();
+
+            var container = builder.Build();
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
 
             return container;
@@ -49,19 +104,19 @@ namespace $rootnamespace$
         /// <summary>
         /// Registers the custome services.
         /// </summary>
-        /// <param name="containerBuilder"></param>
-        private static void InitializeContainer(ContainerBuilder containerBuilder)
+        /// <param name="builder"></param>
+        private static void InitializeContainer(ContainerBuilder builder)
         {
-#error Register your services here (remove this line).
+			#error Register your services here (remove this line).
             // For instance:
-            //containerBuilder.RegisterType<DefaultDbProvider>().As<IDbProvider>();
+            //builder.RegisterType<DefaultDbProvider>().As<IDbProvider>();
 
             //Lazy resolver
-            //containerBuilder.Register(x => x.Resolve<RequestContextBuilder>().Build()).As<IRequestContext>();
-
-
-
+            //builder.Register(x => x.Resolve<RequestContextBuilder>().Build()).As<IRequestContext>();
 
         }
+
+		'Add This Line to the Global.asax in Start()'
+		AutofacInjectorInitializer.Initialize(GlobalConfiguration.Configuration);
     }
 }
